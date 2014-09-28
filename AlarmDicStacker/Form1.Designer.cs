1c2520a9be6af3f2cb133fea3610a29dbf499bfc
Tomoya Yamanaka <kumo203@gmail.com>
2014-09-28 23:03:43 +0900

    First Commit

---
+++ AlarmDicStacker/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace AlarmDicStacker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView = new System.Windows.Forms.DataGridView();
+            this.No = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tag = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Message = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OnMsg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.OffMsg = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Level = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Register = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Flip = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PageJump = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SubScreen = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.folderBrowserDialog = new System.Windows.Forms.FolderBrowserDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 217);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(1065, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "AlarmDic.csv";
+            this.openFileDialog.Filter = "AlarmDic.csv|*.csv";
+            // 
+            // dataGridView
+            // 
+            this.dataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.No,
+            this.Tag,
+            this.Message,
+            this.OnMsg,
+            this.OffMsg,
+            this.Level,
+            this.Register,
+            this.Flip,
+            this.PageJump,
+            this.SubScreen});
+            this.dataGridView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView.Name = "dataGridView";
+            this.dataGridView.RowTemplate.Height = 21;
+            this.dataGridView.Size = new System.Drawing.Size(1065, 217);
+            this.dataGridView.TabIndex = 1;
+            // 
+            // No
+            // 
+            this.No.HeaderText = "No";
+            this.No.Name = "No";
+            this.No.ReadOnly = true;
+            this.No.Width = 40;
+            // 
+            // Tag
+            // 
+            this.Tag.HeaderText = "Tag";
+            this.Tag.Name = "Tag";
+            this.Tag.ReadOnly = true;
+            // 
+            // Message
+            // 
+            this.Message.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Message.HeaderText = "Message";
+            this.Message.Name = "Message";
+            this.Message.ReadOnly = true;
+            // 
+            // OnMsg
+            // 
+            this.OnMsg.HeaderText = "OnMsg";
+            this.OnMsg.Name = "OnMsg";
+            this.OnMsg.ReadOnly = true;
+            // 
+            // OffMsg
+            // 
+            this.OffMsg.HeaderText = "OffMsg";
+            this.OffMsg.Name = "OffMsg";
+            this.OffMsg.ReadOnly = true;
+            // 
+            // Level
+            // 
+            this.Level.HeaderText = "Level";
+            this.Level.Name = "Level";
+            this.Level.ReadOnly = true;
+            this.Level.Width = 80;
+            // 
+            // Register
+            // 
+            this.Register.HeaderText = "Register";
+            this.Register.Name = "Register";
+            this.Register.ReadOnly = true;
+            // 
+            // Flip
+            // 
+            this.Flip.HeaderText = "Filp";
+            this.Flip.Name = "Flip";
+            this.Flip.ReadOnly = true;
+            this.Flip.Width = 40;
+            // 
+            // PageJump
+            // 
+            this.PageJump.HeaderText = "PageJump";
+            this.PageJump.Name = "PageJump";
+            this.PageJump.ReadOnly = true;
+            this.PageJump.Width = 70;
+            // 
+            // SubScreen
+            // 
+            this.SubScreen.HeaderText = "SubScreen";
+            this.SubScreen.Name = "SubScreen";
+            this.SubScreen.ReadOnly = true;
+            this.SubScreen.Width = 70;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1065, 240);
+            this.Controls.Add(this.dataGridView);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "AlarmDicStacker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridView dataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn No;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tag;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Message;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OnMsg;
+        private System.Windows.Forms.DataGridViewTextBoxColumn OffMsg;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Level;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Register;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Flip;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PageJump;
+        private System.Windows.Forms.DataGridViewTextBoxColumn SubScreen;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog;
+    }
+}
+
---
+++ AlarmDicStacker/Form1.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+[assembly: InternalsVisibleTo("AlarmDicStackerUnitTest")]
+
+namespace AlarmDicStacker
+{
+    public partial class Form1 : Form
+    {
+        AlarmDicStacker ads;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ads = new AlarmDicStacker(openFileDialog.FileName);
+
+                foreach ( var line in ads.Fields ){
+                    dataGridView.Rows.Add(line);
+                }
+            }
+            else
+            {
+                this.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                //foreach (var row in dataGridView.Rows.Cast<DataGridViewRow>())
+                //{
+                //    foreach (var cell in row.Cells.Cast<DataGridViewCell>())
+                //    {
+                //        Debug.WriteLine(cell.Value);
+                //    }
+                //}
+
+                string[] subScreens = ads.GetSubData();
+                for (int i=0; i<subScreens.Length; i++){
+                    File.WriteAllText(folderBrowserDialog.SelectedPath+string.Format(@"\SubData{0}.csv", AlarmDicStacker.PAGE_OFFSET+i),
+                        subScreens[i], Encoding.GetEncoding(932));
+                }
+            }
+        }
+    }
+
+    public class AlarmDicStacker
+    {
+        public static readonly int PAGE_OFFSET = 100;
+        static readonly int WIDTH = 1000;
+        static readonly int HEIGHT = 1000;
+        static readonly int W_OFFSET = 200;
+        static readonly int H_OFFSET = 25;
+
+        List<string[]> fields = new List<string[]>();
+
+        public string[][] Fields { get { return fields.ToArray(); } }
+        public AlarmDicStacker(string dic)
+        {
+            using (TextFieldParser tfp = new TextFieldParser(dic, Encoding.GetEncoding(932)))
+            {
+                tfp.Delimiters = new string[] { "," };
+                int step = 0;
+                while (tfp.EndOfData == false && step < 2)
+                {
+                    string[] line = tfp.ReadFields();
+                    if (line[0] == "[アラーム用辞書]")
+                    {
+                        step++;
+                    }
+                    else if (line[0] == "[END]") 
+                    {
+                        step++;
+                    } else {
+                        fields.Add(line);
+                    }
+                }
+            }
+        }
+
+        public string[] GetSubData()
+        {
+            int pageNo = 0;
+            List<string> results = new List<string>();
+            StringBuilder result = new StringBuilder();
+
+            SetHeader(pageNo, result);
+            int h_move = H_OFFSET;
+            int w_move = W_OFFSET;
+            for (int i = 0; i < Fields.Length; i++)
+            {
+                if (h_move >= HEIGHT)
+                {
+                    h_move = H_OFFSET;
+                    w_move += W_OFFSET;
+                    if (w_move >= WIDTH)
+                    {
+                        w_move = W_OFFSET;
+                        results.Add(result.ToString());
+                        result.Clear();
+                        pageNo++;
+                        SetHeader(pageNo, result);
+                    }
+                }
+
+                result.Append(string.Format(@"STR,{0},{1},0,0,255,,5,16777215,16777215,1,{2},", 
+                    w_move, h_move, Fields[i][2]));
+                result.Append(@"Reserved1,,Reserved2,Reserved3,,-16,0,0,0,400,0,0,0,128,3,2,1,17,ＭＳ 明朝,0,16777215,16777215,0,単位,20,");
+                result.Append(string.Format(@"0,0,0,0,0,0,0,128,0,0,0,0,ＭＳ 明朝,if([{0}]){1}", Fields[i][1], "{COLORCHANGE()},END,0"));
+                result.AppendLine();
+
+                h_move += H_OFFSET;
+            }
+
+            if (result.ToString() != "")
+            {
+                results.Add(result.ToString());
+            }
+
+            return results.ToArray();
+        }
+
+        private static void SetHeader(int pageNo, StringBuilder result)
+        {
+            result.AppendLine(string.Format(@"Page,{0},サブスクリーン {0},0,,1000,1000,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,",
+                pageNo + PAGE_OFFSET));
+        }
+    }
+}
